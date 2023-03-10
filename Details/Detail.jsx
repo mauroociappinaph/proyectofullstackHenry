6637c9250a682bc1828d2e2b84deda06f27ff87f
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2023-03-10 18:03:51 -0300

    Add files via upload

--- Details/Detail.jsx
+++ Details/Detail.jsx
@@ -1,1 +1,17 @@
-import styled from '../Details/DetailsModule.css';
+import React, { useState, useParams } from 'react'; 
+import styled from './Detail.Module.css';
+
+
+
+class Details extends React.Component {
+render() {
+      return (
+        <div className={styled.div}>
+          <h1>About</h1>
+          <p>This is the about page.</p>
+        </div>
+      );
+    }
+  }
+  
+  export default Details;
